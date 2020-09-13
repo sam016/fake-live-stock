5fb2e772c48f045aa9e5e55b1b3ec2379058e019
sam016 <devmaster@sam016.com>
2020-09-13 21:34:12 +0530

    Update express app for websocket

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,16 @@
 import express from 'express';
+import path from 'path';
 import socketIO from 'socket.io';
 import { getRandomData } from './stocks';
 
 const port = process.env.PORT || 4000;
 const app = express();
 app.set('port', port);
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 const http = require('http').Server(app);
 
